738de4ff294387a466bf7146e69436c567cdddb9
Adebimpe Adebowale <adeadebimpe96@gmail.com>
2020-12-10 18:33:47 +0100

    fixed header

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ let toggle = () =>{
     let toggleBar = document.querySelector('.toggle-menu')
     let sideBar = document.querySelector('.menu-nav')
     let contianer = document.querySelector('.nav')
-
     
     toggleBar.addEventListener('click', ()=>{
         contianer.classList.toggle('active')
